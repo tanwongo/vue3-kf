a2e9c5ac02ff451e2cf5fcffc080871ad14097f2
jiangxin <jiangxin123@hotmail>
2022-02-16 18:43:22 +0800

    1

---
+++ src/config/api.ts
@@ -0,0 +1,20 @@
+
+export const Api = {
+    BASE_URL: "https://cs4.17996api.net/",
+    // 172.26.205.166: 8001
+    // https://cs4.17996api.net
+    HOME_FAQ_LIST:"/faq/list/", //FAQ
+    T_CUSTOMER_SERVICE:"/appinfo?appid=",//第三方客服
+    MY_QUESTION:"/posts/list", //我的问题
+    DETAIL_QUESTION:"/posts/detail", //问题详情
+    CREATE_QUESTION:"/posts/create", //创建问题
+    REPLY_QUESTION:"/posts/reply", //回复问题
+    CLOSE_QUESTION:"/posts/close", //关闭问题
+    IMAGES_CDN:"https://cs.17996cdn.com", //图片地址
+    AUTH:{
+        k:"Authorization",
+        v: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIwMDAwMTU1LCJ0aW1lIjoxNTk3ODIwODY1LCJkZXZpY2VpZCI6IjQyNkRDQjIwLUM5QjEtNDZBQS05MjVBLTEyNDU3NDgwNDE4NSIsImd1aWQiOiI0MjZEQ0IyMC1DOUIxLTQ2QUEtOTI1QS0xMjQ1NzQ4MDQxODUiLCJhcHBpZCI6IjEwMDAwMDAwIiwiZXhwaXJlIjoxNTk4NDI1NjY1fQ.ZIZj2tn_BWlynfkn1Px8r5r70oXqxZsgUIOvquUEbFA"
+    }
+}
+
+export default Api
---
+++ src/views/MyQuestion.vue
@@ -0,0 +1,103 @@
+<template>
+<van-nav-bar :title="t('HeadMyQ')" class="my-head-title" safe-area-inset-top />
+<div class="pdb4">
+    <div v-for="(v,k) in list" :key="k">
+        <div class="my-question-c">
+            <van-button type="default" @click="questionRequest(v)">
+                <template v-if="v.status !== 3 || v.status !== 10 || v.status !== 11">
+                    <span class="txt">{{v.title}}<van-image width="1rem" height="1rem" class="cimg" fit="contain" src="/src/assets/exchange.png"/></span>
+                </template>
+                <template v-else>
+                    <span class="txt">{{v.title}}<van-image width="1rem" height="1rem" class="cimg" fit="contain" src="/src/assets/complete.png"/></span>
+                </template>
+                <br>
+                <p class="time">{{MomentJs.unix(v.updated_at).format("YYYY-MM-DD HH:mm:ss")}}</p>
+            </van-button>
+        </div>
+    </div>
+</div>
+<div v-if="noQuestion" class="noQuestion">
+    <p>{{t('createOne')}}</p>
+    <van-button icon="comment-o" type="primary" @click="submitQuestion()">{{t('create')}}</van-button>
+</div>
+</template>
+<script lang="ts" setup>
+import { defineComponent,ref,onMounted } from 'vue'
+import { useI18n } from "vue-i18n";
+import {SDK_VALUE,requestGet}  from '../utils/request'
+import {Api} from '../config/api'
+import Moment from 'moment'
+import {Toast}  from 'vant'
+import {useRouter } from 'vue-router'
+
+const { t } = useI18n();
+const list = ref([])
+const MomentJs = Moment
+const router = useRouter()
+const SDK_V:any = SDK_VALUE()
+const noQuestion = ref(false)
+
+onMounted(async () => {
+    SDK_V.then((v:any)=>{
+        let _url = Api.MY_QUESTION
+        if(v.guid){
+            _url +="?uid="+v.guid
+        }
+         requestGet(_url).then((res:any)=>{
+             if(res.data.data.length===0 && res.data.code === 0){
+                 noQuestion.value = true
+             }
+             else{
+                 list.value = res.data.data
+             }
+             Toast.clear()
+        })
+    })
+})
+
+const questionRequest = (v:any)=> {
+       router.push({
+            name:"QuestionRequest",
+            params:{
+                id:v.id,
+                typeid:v.typeid
+            }
+        })
+}
+
+const submitQuestion = ()=> {
+    router.push({
+        name:"SubmitQuestion",
+        params:{
+            id:-1,
+            typeid:0,
+            isreply:-1
+        }
+    })
+}
+
+</script>
+<style lang="less" scoped>
+.my-head-title {
+    padding-left: 10px;
+}
+.my-question-c{
+    position: relative;
+    margin-bottom: 10px;
+    border-radius: 3px;
+    .txt{
+        margin-top: 10px;
+        display: inline-block;
+    }
+    .cimg{
+        vertical-align: bottom;
+        margin-left: 10px;
+    }
+    .time{
+        font-size: 12px;
+    }
+}
+.noQuestion{
+    text-align: center;
+}
+</style>
